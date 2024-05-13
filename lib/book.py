f629a787b71bb2601a916f0cb552e37484d75cae
Kevin Choi <kevinnwjchoi@gmail.com>
2024-05-13 19:00:54 -0400

    complete lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
     
-        
+    def __init__(self, title, page_count=0):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        """The page count property"""
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):
+        '''prints "page_count must be an integer" if page_count is not an integer.'''
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand, size=0, condition="used"):
+        self._size = size
+        self._brand = brand
+        self._condition = condition
+
+    @property
+    def brand(self):
+        """This is the brand property"""
+        return self._brand
+    
+    @property
+    def size(self):
+        """This is the size property"""
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self.size = size
+        else: 
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+
+    condition = "New"
